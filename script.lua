98e192cf86dfab6f7e0d8981e0ab7f75208295ef
navet <83888350+GTX1O8OTi@users.noreply.github.com>
2021-08-22 04:59:07 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,8 @@
-print("another try")
+local hui = get_hidden_gui or gethui
+
 local players = game.Players
 local lp = players.LocalPlayer
 local lpChar = lp.Character or lp.CharacterAdded:Wait()
-
-local setc = setclipboard or toclipboard or set_clipboard
 
 local debugging = false
 
@@ -25,6 +24,9 @@ local commands = {
 			setc(v)
 		end
 	end,
+	["jppower"] = function(arg1)
+		lpChar.Humanoid.JumpPower = tonumber(arg1)
+	end,
 }
 
 
@@ -32,29 +34,58 @@ local commands = {
 -- [[ COMANDOS ]] --
 
 local function RCMD(cmd)
-local index = 0
+	local index = 0
 
-local comando, args
+	local comando, args
 
-for result in string.gmatch(cmd, "%g+") do
+	for result in string.gmatch(cmd, "%g+") do
 
-if debugging then
-print (result, index)
+		if debugging then
+			print (result, index)
+		end
+
+		if index == 0 and commands[result] then
+			comando = result
+
+		elseif index == 1 then
+			args = result
+			break
+		end
+
+		index = index + 1
+	end
+
+	commands[comando](args)
+
 end
 
-if index == 0 and commands[result] then
-comando = result
+local sc = Instance.new("ScreenGui")
 
-elseif index == 1 then
-args = result
-break
+if hui then
+sc.Parent = hui()
+else sc.Parent = game.CoreGui
 end
 
-index = index + 1
-end
+local HTTPService = game:GetService("HttpService")
 
-commands[comando](args)
+sc.Name = HTTPService:GenerateGUID(false)
+sc.ResetOnSpawn = false
 
-end
+local textbox = Instance.new("TextBox")
+textbox.BackgroundColor3 = Color3.fromRGB(255,255,255)
+textbox.ClearTextOnFocus = true
+textbox.Size = UDim2.new(0,200,0,50)
+textbox.Position = UDim2.new(0,0,0.736,0)
+textbox.Parent = sc
 
-lp.Chatted:Connect(RCMD)
+local CAS = game:GetService("ContextActionService")
+
+CAS:BindAction("focus", function()
+	textbox:CaptureFocus()
+end, false, Enum.KeyCode.RightBracket)
+
+textbox.FocusLost:Connect(function(enter)
+	if enter then
+		RCMD(textbox.Text)
+	end
+end)
