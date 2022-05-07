0a55755fe9a8013e6bc6559bd92956c027215278
navet <83888350+GTX1O8OTi@users.noreply.github.com>
2022-05-07 20:51:28 +0000

    Delete functions.lua

--- functions.lua
+++
@@ -1,8 +0,0 @@
-function GetPlayer(PLAYER_NAME)
-	for i,v in pairs (Players:GetPlayers()) do
-		if string.lower(PLAYER_NAME):match("^" .. string.lower(v.Name)) then
-			return v
-		end
-	end
-	return nil
-end
